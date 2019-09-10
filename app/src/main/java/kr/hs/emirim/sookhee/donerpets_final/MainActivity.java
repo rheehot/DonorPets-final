ac53f97c0357780cb2d61bd453a92e344ad14d35
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-09-11 03:19:04 +0900

    Rebuild the repositories

--- app/src/main/java/kr/hs/emirim/sookhee/donerpets_final/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sookhee/donerpets_final/MainActivity.java
@@ -1,6 +1,7 @@
 package kr.hs.emirim.sookhee.donerpets_final;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
