72688069cc6d6fd9883863d986a12694f7b91f1e
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-09-18 16:15:27 +0900

    2019.09.17

--- app/src/main/java/kr/hs/emirim/sookhee/donerpets_final/DetailActivity.java
+++ app/src/main/java/kr/hs/emirim/sookhee/donerpets_final/DetailActivity.java
@@ -24,6 +24,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.NavigableMap;
 import java.util.Random;
 
 public class DetailActivity extends AppCompatActivity {
@@ -42,6 +43,7 @@ public class DetailActivity extends AppCompatActivity {
     FirebaseDatabase Storydatabase;
     DatabaseReference myRefStory;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,7 +52,7 @@ public class DetailActivity extends AppCompatActivity {
         DataApplication MyData = (DataApplication)getApplication();
 
         Storydatabase = FirebaseDatabase.getInstance();
-        myRefStory = Storydatabase.getReference("story").child(String.valueOf(MyData.getShelterPosition()));
+        myRefStory = Storydatabase.getReference("story").child(String.valueOf(MyData.getStoryPosition()));
 
         myRefStory.addValueEventListener(new ValueEventListener() {
             @Override
@@ -58,23 +60,39 @@ public class DetailActivity extends AppCompatActivity {
                 String title = dataSnapshot.child("title").getValue(String.class);
                 String img1 = dataSnapshot.child("img1").getValue(String.class);
                 String img2 = dataSnapshot.child("img2").getValue(String.class);
+                String img3 = dataSnapshot.child("img3").getValue(String.class);
+                String img4 = dataSnapshot.child("img4").getValue(String.class);
                 String line1 = dataSnapshot.child("line1").getValue(String.class);
                 String line2 = dataSnapshot.child("line2").getValue(String.class);
+                String line3 = dataSnapshot.child("line3").getValue(String.class);
+                String line4 = dataSnapshot.child("line4").getValue(String.class);
                 String date = dataSnapshot.child("date").getValue(String.class);
 
                 //데이터를 화면에 출력해 준다.
                 TextView Title = findViewById(R.id.title_activity2);
                 Title.setText(title);
+
+                TextView Date = findViewById(R.id.text_date);
+                Date.setText(date);
+
                 ImageView Img1 = (ImageView) findViewById(R.id.image1_activity2);
                 Picasso.get().load(img1).into(Img1);
                 ImageView Img2 = (ImageView) findViewById(R.id.image2_activity2);
                 Picasso.get().load(img2).into(Img2);
+                ImageView Img3 = (ImageView) findViewById(R.id.image3_activity2);
+                Picasso.get().load(img3).into(Img3);
+                ImageView Img4 = (ImageView) findViewById(R.id.image4_activity2);
+                Picasso.get().load(img4).into(Img4);
+
                 TextView Line1 = findViewById(R.id.text1_activity2);
                 Line1.setText(line1);
                 TextView Line2 = findViewById(R.id.text2_activity2);
                 Line2.setText(line2);
-                TextView Date = findViewById(R.id.text_date);
-                Date.setText(date);
+                TextView Line3 = findViewById(R.id.text3_activity2);
+                Line3.setText(line3);
+                TextView Line4 = findViewById(R.id.text4_activity2);
+                Line4.setText(line4);
+
             }
 
             @Override
