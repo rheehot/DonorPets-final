e12033bfbf797072214f85558a45859f043414fc
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-09-24 18:10:15 +0900

    2019.09.24 backup

--- app/src/main/java/kr/hs/emirim/sookhee/donerpets_final/ProfileActivity.java
+++ app/src/main/java/kr/hs/emirim/sookhee/donerpets_final/ProfileActivity.java
@@ -4,22 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.util.Printer;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -29,6 +16,8 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        TextView username = (TextView)findViewById(R.id.profile_name);
+        username.setText(SaveSharedPreference.getUserName(ProfileActivity.this));
     }
 
     public void onBackClick(View v){
--- app/src/main/java/kr/hs/emirim/sookhee/donerpets_final/SettingActivity.java
+++ app/src/main/java/kr/hs/emirim/sookhee/donerpets_final/SettingActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.sookhee.donerpets_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -10,12 +11,12 @@ import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
 
-    DataApplication MyData = (DataApplication) getApplication();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+
 
     }
 
